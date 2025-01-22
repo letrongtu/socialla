543215675d8c6e7fd34691a16926ad8bae128949
Trong Tu Le <letrongtu203@gmail.com>
2025-01-22 18:48:54 +0800

    Media Upload APIs and frontend

---
+++ frontend/src/features/media-upload/api/use-media-local-upload.ts
@@ -0,0 +1,77 @@
+import axios, { AxiosError } from "axios";
+import { useMemo, useState } from "react";
+
+const baseURL = "http://localhost:5096/api";
+
+type RequestType = {
+  files: File[];
+  userId: string;
+};
+
+type ResponeType = {
+  results: {
+    FileName: string;
+    FileUrl: string;
+  }[];
+} | null;
+
+type Options = {
+  onSuccess?: (data: ResponeType) => void;
+  onError?: (error: AxiosError) => void;
+  onSettled?: () => void;
+};
+
+export const UseMediaLocalUpload = () => {
+  const [data, setData] = useState<ResponeType>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [status, setStatus] = useState<
+    "success" | "error" | "pending" | "settled" | null
+  >(null);
+
+  const isPending = useMemo(() => status === "pending", [status]);
+  const isSuccess = useMemo(() => status === "success", [status]);
+  const isError = useMemo(() => status === "error", [status]);
+  const isSettled = useMemo(() => status === "settled", [status]);
+
+  const localUploadMedia = async (values: RequestType, options?: Options) => {
+    try {
+      setData(null);
+      setError(null);
+      setStatus("pending");
+
+      const formData = new FormData();
+      values.files.forEach((file) => {
+        formData.append("files", file);
+      });
+
+      formData.append("userId", values.userId);
+
+      // formData.append("userId", values.userId || "");
+
+      const response = await axios.post<ResponeType>(
+        `${baseURL}/media/multi-upload`,
+        formData
+      );
+
+      options?.onSuccess?.(response.data);
+
+      return response;
+    } catch (error) {
+      setStatus("error");
+      options?.onError?.(error as AxiosError);
+    } finally {
+      setStatus("settled");
+      options?.onSettled?.();
+    }
+  };
+
+  return {
+    localUploadMedia,
+    data,
+    error,
+    isPending,
+    isSuccess,
+    isError,
+    isSettled,
+  };
+};
--- frontend/src/features/posts/components/create-post-modal.tsx
+++ frontend/src/features/posts/components/create-post-modal.tsx
@@ -5,6 +5,8 @@ const CreatePostEditor = dynamic(
   { ssr: false }
 );
 
+import { useRef, useState } from "react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useCreatePostModal } from "../store/use-create-post-modal";
 import { useCurrentUser } from "@/features/auth/api/use-current-user";
@@ -19,17 +21,24 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-import { Globe } from "lucide-react";
+import { Globe, ImagePlusIcon, X } from "lucide-react";
 import { IoCaretDown } from "react-icons/io5";
 import { FaPhotoVideo } from "react-icons/fa";
 import { Hint } from "@/components/ui/hint";
+import { UseMediaLocalUpload } from "@/features/media-upload/api/use-media-local-upload";
+import { toast } from "sonner";
 
 export const CreatePostModal = () => {
   const router = useRouter();
 
   const [open, setOpen] = useCreatePostModal();
-
   const { data } = useCurrentUser();
+  const { localUploadMedia, isPending } = UseMediaLocalUpload();
+
+  const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
+  const [dragActive, setDragActive] = useState(false);
+
+  const imageElementRef = useRef<HTMLInputElement>(null);
 
   if (!data) {
     return null;
@@ -39,6 +48,59 @@ export const CreatePostModal = () => {
 
   const avatarFallback = firstName?.charAt(0).toUpperCase();
   const placeholder = `What's on your mind, ${firstName}?`;
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragActive(true);
+  };
+
+  const handleDragLeave = () => {
+    setDragActive(false);
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const files = Array.from(e.dataTransfer.files);
+    setUploadedFiles((prev) => [...prev, ...files]);
+  };
+
+  const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = Array.from(e.target.files || []);
+    setUploadedFiles((prev) => [...prev, ...files]);
+
+    e.target.value = "";
+  };
+
+  const handleSubmit = () => {
+    localUploadMedia(
+      { files: uploadedFiles, userId: id ? id : "" },
+      {
+        onSuccess: (response) => {
+          console.log(response);
+          toast.success("Files uploaded successfully!");
+        },
+        onError: (error) => {
+          toast.error("File upload failed.");
+          console.error(error);
+        },
+        // onError: (error) => {
+        //   if (Array.isArray(error.response?.data)) {
+        //     const errorMessages = error.response?.data.map(
+        //       (error) => error.description
+        //     );
+        //     setErrorMessages(errorMessages);
+        //   } else {
+        //     setErrorMessages([error.response?.data as string]);
+        //   }
+        // },
+        // onSettled: () => {
+        //   setTimeout(() => {
+        //     setErrorMessages(null);
+        //   }, 5000);
+        // },
+      }
+    );
+  };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -79,20 +141,87 @@ export const CreatePostModal = () => {
 
         <CreatePostEditor placeholder={placeholder} />
 
+        <div
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+          className="w-full flex flex-col justify-center items-center rounded-lg border"
+        >
+          <input
+            ref={imageElementRef}
+            onChange={handleFileSelect}
+            type="file"
+            multiple
+            className="hidden"
+          />
+
+          <div className="relative w-full max-w-[calc(100%-1rem)] h-60 m-2 rounded-lg flex flex-col items-center justify-center bg-[#c9ccd1]/30 hover:bg-[#c9ccd1]/60 cursor-pointer">
+            {uploadedFiles.length === 0 ? (
+              <div
+                onClick={() => imageElementRef.current?.click()}
+                className="flex flex-col items-center justify-center "
+              >
+                <div className="flex items-center justify-center w-12 h-12 rounded-full mb-1 bg-[#c9ccd1]/70">
+                  <ImagePlusIcon className="w-6 h-6 text-black" />
+                </div>
+                <p className="text-base font-semibold">Add photos/videos</p>
+                <p className="text-xs text-muted-foreground">
+                  or drag and drop
+                </p>
+              </div>
+            ) : (
+              <div className="flex flex-col items-center justify-center">
+                {uploadedFiles.map((file, index) => {
+                  const filePreviewUrl = URL.createObjectURL(file);
+                  return (
+                    <div
+                      key={index}
+                      className="w-full aspect-square overflow-hidden rounded-lg"
+                    >
+                      <Image
+                        fill
+                        src={filePreviewUrl}
+                        alt={`Uploaded ${index + 1}`}
+                        className="object-cover w-full h-full"
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+
+            <div
+              onClick={() => {
+                setUploadedFiles([]);
+              }}
+              className="absolute top-2 right-2 flex items-center justify-center w-8 h-8 rounded-full mb-1 bg-[#c9ccd1]/70 cursor-pointer"
+            >
+              <X className="w-6 h-6 text-black" />
+            </div>
+          </div>
+        </div>
+
         <div className="w-full h-14 flex justify-between items-center rounded-lg border hover:cursor-pointer">
           <p className="text-base font-semibold ml-4">Add to your post</p>
           <div className="flex space-x-2 mr-4">
             <Hint label="Photo/Video">
-              <button className="p-1.5 rounded-md hover:bg-[#c9ccd1]/30">
+              <button
+                onClick={() => {}}
+                className="p-1.5 rounded-md hover:bg-[#c9ccd1]/30"
+              >
                 <FaPhotoVideo className="size-6 text-green-600" />
               </button>
             </Hint>
           </div>
         </div>
 
-        <div className="w-full h-10 flex justify-center items-center bg-[#283959] rounded-lg hover:cursor-pointer">
-          <button className="text-lg font-semibold text-white">Post</button>
-        </div>
+        <button
+          onClick={handleSubmit}
+          disabled={isPending}
+          className="w-full h-10 flex justify-center items-center bg-[#283959] rounded-lg hover:cursor-pointer"
+        >
+          <p className="text-lg font-semibold text-white">Post</p>
+        </button>
       </DialogContent>
     </Dialog>
   );
